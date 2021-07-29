8bfe971cb98d7a61114153df6740e1042b60332b
GitHub Action <action@github.com>
2021-07-29 10:13:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/renegust/portaldevhelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/renegust/portaldevhelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.renegust.portaldevhelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.renegust.portaldevhelper.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/renegust/portaldevhelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/renegust/portaldevhelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.renegust.portaldevhelper.services
 
+import com.github.renegust.portaldevhelper.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
